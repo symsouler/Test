26ec8090fc4a2f909a05dae1d3332367e25b2d20
symsouler <symsouler@163.com>
2020-08-28 22:11:42 +0800

    硬件接口

---
+++ src/com/boot/HardWareApi.java
@@ -0,0 +1,11 @@
+package src.com.boot;
+
+/**
+ * 2  * @Description :
+ * 3  * @Author : 作者  2020/8/28 0028 22:10
+ * 4
+ */
+public interface HardWareApi {
+    void connect();
+    void disConnect();
+}
