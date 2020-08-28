5751374585ddea4cef7975b00d93d144ff9186d3
symsouler <symsouler@163.com>
2020-08-28 22:30:51 +0800

    总线接口

---
+++ src/com/boot/BusLockApi.java
@@ -0,0 +1,10 @@
+package src.com.boot;
+
+/**
+ * 2  * @Description :
+ * 3  * @Author : 作者  2020/8/28 0028 22:29
+ * 4
+ */
+public interface BusLockApi extends HardWareApi{
+    void combine();
+}
